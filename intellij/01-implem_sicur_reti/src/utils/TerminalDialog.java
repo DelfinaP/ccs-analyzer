1f05153d8a259872fb920ce1358eebc2d0df501c
Alessandro Polisena <ale.polisena@gmail.com>
2019-08-28 13:23:00 +0200

    Prosecuzione implementazione dell'interrogazione del terminale

--- intellij/01-implem_sicur_reti/src/utils/TerminalDialog.java
+++ intellij/01-implem_sicur_reti/src/utils/TerminalDialog.java
@@ -2,9 +2,18 @@ package utils;
 
 import java.io.*;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class TerminalDialog {
     Process process;
+    List<String> terminalOutputStrings;
+    OutputStream stdin;
+    InputStream stderr;
+    InputStream stdout;
+
+    BufferedReader reader;
+    BufferedWriter writer;
 
     Timestamp time1;
     Timestamp time2;
@@ -21,12 +30,35 @@ public abstract class TerminalDialog {
 
         avviaTerminale();
 
-        OutputStream stdin = process.getOutputStream();
-        InputStream stderr = process.getErrorStream();
-        InputStream stdout = process.getInputStream();
+        stdin = process.getOutputStream();
+        stderr = process.getErrorStream();
+        stdout = process.getInputStream();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+        reader = new BufferedReader(new InputStreamReader(stdout));
+        writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+
+        terminalOutputStrings = getTerminalOutput("cd ~");
+
+//        Thread readBufferThread = new Thread() {
+//            public void run() {
+//                try {
+//                    System.out.println("Does it work?");
+//
+//                    Thread.sleep(1000);
+//
+//                    System.out.println("Nope, it doesnt...again.");
+//                } catch(InterruptedException v) {
+//                    System.out.println(v);
+//                }
+//            }
+//        };
+
+        // Stampiamo la lista
+
+        for (int i = 0; i < terminalOutputStrings.size(); i++) {
+            System.out.println(terminalOutputStrings.get(i));
+        }
 
 //        String input = "cd ~";
 //        input += "\n";
@@ -43,19 +75,111 @@ public abstract class TerminalDialog {
 //        writer.write(input);
 //        writer.flush();
 
+//        time1 = new Timestamp(System.currentTimeMillis());
+//        time2 = new Timestamp(System.currentTimeMillis());
+//
+//        time1millis = time1.getTime();
+//        time2millis = time2.getTime();
+//
+//        diffTimeMillis = time2millis - time1millis;
+//
+//        while ((line = reader.readLine()) != null) {
+//            if (line.length() == 0) {
+//                break;
+//            }
+//            System.out.println("Stdout: " + line);
+//            time2 = new Timestamp(System.currentTimeMillis());
+//            time2millis = time2.getTime();
+//            diffTimeMillis = time2millis - time1millis;
+    }
+
+    private ArrayList<String> getTerminalOutput(String command) throws IOException {
+        ArrayList<String> stringList = new ArrayList<String>();
+
+        command += "\n";
+        writer.write(command);
+        writer.flush();
+
+        Thread readBufferThread = new Thread() {
+            public void run() {
+                try {
+                    String line;
+                    // Leggiamo con readLine() e aggiungiamo alla lista
+                    System.out.println("Primo thread");
+                    while ((line = reader.readLine()) != null) {
+                        stringList.add(line);
+                        System.out.println("Stdout: " + line);
+                    }
+                    // Thread.sleep(10000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread interruptBufferThread = new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("Secondo thread");
+                    reader.close();
+                    System.out.println("Chiusura stream 'reader' completata");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        readBufferThread.start();
+        interruptBufferThread.start();
+
+        // Busy waiting di 1 secondo
         time1 = new Timestamp(System.currentTimeMillis());
         time2 = new Timestamp(System.currentTimeMillis());
 
         time1millis = time1.getTime();
         time2millis = time2.getTime();
 
-        diffTimeMillis = time2millis - time1millis;
-
-        while (diffTimeMillis < 10 && (line = reader.readLine()) != null) {
-            System.out.println("Stdout: " + line);
+        while (diffTimeMillis < 1000) {
+            time2 = new Timestamp(System.currentTimeMillis());
+            time2millis = time2.getTime();
+            diffTimeMillis = time2millis - time1millis;
         }
+
+        readBufferThread.interrupt();
+        interruptBufferThread.interrupt();
+
+        // Busy waiting di 200 ms
+        time1 = new Timestamp(System.currentTimeMillis());
+        time2 = new Timestamp(System.currentTimeMillis());
+
+        time1millis = time1.getTime();
+        time2millis = time2.getTime();
+
+        while (diffTimeMillis < 200) {
+            time2 = new Timestamp(System.currentTimeMillis());
+            time2millis = time2.getTime();
+            diffTimeMillis = time2millis - time1millis;
+        }
+
+        // Verifichiamo che i thread siano interrotti
+        if (readBufferThread.isInterrupted()) {
+            System.out.println("Il thread 'readBufferThread' è interrotto");
+        }
+        else {
+            System.out.println("Il thread 'readBufferThread' non è interrotto");
+        }
+        if (interruptBufferThread.isInterrupted()) {
+            System.out.println("Il thread 'interruptBufferThread' è interrotto");
+        }
+        else {
+            System.out.println("Il thread 'interruptBufferThread' non è interrotto");
+        }
+
+        return stringList;
     }
 
     protected abstract void avviaTerminale() throws IOException;
-
 }
