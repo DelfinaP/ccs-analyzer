feea3a1a0e8574e6cec9a99f9e11f6ec2b094fe5
DelfinaP <32596674+DelfinaP@users.noreply.github.com>
2019-09-03 10:50:25 +0200

    effettuat rimozione codice inutile

--- intellij/01-implem_sicur_reti/src/utils/TerminalDialog.java
+++ intellij/01-implem_sicur_reti/src/utils/TerminalDialog.java
@@ -2,13 +2,11 @@ package utils;
 
 import java.io.*;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public abstract class TerminalDialog {
     Process process;
-    List<String> terminalOutputStrings;
     OutputStream stdin;
     InputStream stderr;
     InputStream stdout;
@@ -24,7 +22,6 @@ public abstract class TerminalDialog {
      * Questo metodo implementa il design pattern TEMPLATE METHOD
      */
     public void run() throws IOException {
-        String line;
         stringList = new LinkedList<String>();
 
         avviaTerminale();
@@ -44,32 +41,6 @@ public abstract class TerminalDialog {
         stringheEstratte = consumaLista();
 
         stampaStringList(stringheEstratte);
-
-//        elaboraCwb() {
-//            LinkedList<String> mieStringhe = consumaLista() {
-//            }
-//        }
-
-//        Thread readBufferThread = new Thread() {
-//            public void run() {
-//                try {
-//                    System.out.println("Does it work?");
-//
-//                    Thread.sleep(1000);
-//
-//                    System.out.println("Nope, it doesnt...again.");
-//                } catch(InterruptedException v) {
-//                    System.out.println(v);
-//                }
-//            }
-//        };
-
-        // Stampiamo la lista
-
-//        String input = "cd ~";
-//        input += "\n";
-//        writer.write(input);
-//        writer.flush();
     }
 
     private void startReadThread() {
@@ -78,12 +49,10 @@ public abstract class TerminalDialog {
                 try {
                     String line;
                     // Leggiamo con readLine() e aggiungiamo alla lista
-                    System.out.println("Primo thread");
                     while ((line = reader.readLine()) != null) {
                         stringList.add(line);
                         System.out.println("Stdout: " + line);
                     }
-                    // Thread.sleep(10000);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
