eeffa95c0a7ba4c77d984420543ab99fc3bad5b7
Alessandro Polisena <ale.polisena@gmail.com>
2019-09-03 10:31:30 +0200

    Implementata lettura del buffer con inserimento nella LinkedList

--- intellij/01-implem_sicur_reti/src/utils/TerminalDialog.java
+++ intellij/01-implem_sicur_reti/src/utils/TerminalDialog.java
@@ -3,6 +3,7 @@ package utils;
 import java.io.*;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class TerminalDialog {
@@ -15,18 +16,16 @@ public abstract class TerminalDialog {
     BufferedReader reader;
     BufferedWriter writer;
 
-    Timestamp time1;
-    Timestamp time2;
-
-    long time1millis;
-    long time2millis;
-    long diffTimeMillis;
+    LinkedList<String> stringList;
+    LinkedList<String> stringheEstratte;
+    Thread readBufferThread;
 
     /**
      * Questo metodo implementa il design pattern TEMPLATE METHOD
      */
     public void run() throws IOException {
         String line;
+        stringList = new LinkedList<String>();
 
         avviaTerminale();
 
@@ -37,8 +36,19 @@ public abstract class TerminalDialog {
         reader = new BufferedReader(new InputStreamReader(stdout));
         writer = new BufferedWriter(new OutputStreamWriter(stdin));
 
+        startReadThread();
 
-        terminalOutputStrings = getTerminalOutput("cd ~");
+        executeTerminalCommand("cd ~");
+        executeTerminalCommand("ls -l");
+
+        stringheEstratte = consumaLista();
+
+        stampaStringList(stringheEstratte);
+
+//        elaboraCwb() {
+//            LinkedList<String> mieStringhe = consumaLista() {
+//            }
+//        }
 
 //        Thread readBufferThread = new Thread() {
 //            public void run() {
@@ -56,51 +66,14 @@ public abstract class TerminalDialog {
 
         // Stampiamo la lista
 
-        for (int i = 0; i < terminalOutputStrings.size(); i++) {
-            System.out.println(terminalOutputStrings.get(i));
-        }
-
 //        String input = "cd ~";
 //        input += "\n";
 //        writer.write(input);
 //        writer.flush();
-//
-//        input = "ls -l";
-//        input += "\n";
-//        writer.write(input);
-//        writer.flush();
-
-//        String input = "cd ~";
-//        input += "\n";
-//        writer.write(input);
-//        writer.flush();
-
-//        time1 = new Timestamp(System.currentTimeMillis());
-//        time2 = new Timestamp(System.currentTimeMillis());
-//
-//        time1millis = time1.getTime();
-//        time2millis = time2.getTime();
-//
-//        diffTimeMillis = time2millis - time1millis;
-//
-//        while ((line = reader.readLine()) != null) {
-//            if (line.length() == 0) {
-//                break;
-//            }
-//            System.out.println("Stdout: " + line);
-//            time2 = new Timestamp(System.currentTimeMillis());
-//            time2millis = time2.getTime();
-//            diffTimeMillis = time2millis - time1millis;
     }
 
-    private ArrayList<String> getTerminalOutput(String command) throws IOException {
-        ArrayList<String> stringList = new ArrayList<String>();
-
-        command += "\n";
-        writer.write(command);
-        writer.flush();
-
-        Thread readBufferThread = new Thread() {
+    private void startReadThread() {
+        readBufferThread = new Thread() {
             public void run() {
                 try {
                     String line;
@@ -117,69 +90,71 @@ public abstract class TerminalDialog {
             }
         };
 
-        Thread interruptBufferThread = new Thread() {
+        readBufferThread.start();
+    }
+
+    private void executeTerminalCommand(String command) throws IOException {
+        command += "\n";
+        writer.write(command);
+        writer.flush();
+    }
+
+    protected abstract void avviaTerminale() throws IOException;
+
+    private LinkedList<String> consumaLista() {
+        LinkedList<String> stringheEstratte = new LinkedList<String>();
+
+        Thread readBufferThread = new Thread() {
             public void run() {
-                try {
-                    Thread.sleep(1000);
-                    System.out.println("Secondo thread");
-                    reader.close();
-                    System.out.println("Chiusura stream 'reader' completata");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                Timestamp time1 = new Timestamp(System.currentTimeMillis());
+                Timestamp time2 = new Timestamp(System.currentTimeMillis());
+
+                long time1millis = time1.getTime();
+                long time2millis = time2.getTime();
+                long diffTimeMillis = time2millis - time1millis;
+
+                boolean continuaConsumo = true;
+                boolean isVuota = false;
+
+                while (continuaConsumo) {
+                    if (stringList.size() > 0) {
+                        stringheEstratte.add(stringList.remove());
+                        isVuota = false;
+                    }
+
+                    if (stringList.size() == 0 && !isVuota) {
+                        time1 = new Timestamp(System.currentTimeMillis());
+                        time1millis = time1.getTime();
+                        isVuota = true;
+                    }
+
+                    if (stringList.size() == 0 && isVuota) {
+                        time2 = new Timestamp(System.currentTimeMillis());
+                        time2millis = time2.getTime();
+                        diffTimeMillis = time2millis - time1millis;
+                    }
+
+                    if (diffTimeMillis > 200) {
+                        continuaConsumo = false;
+                    }
                 }
+
+                this.interrupt();
             }
         };
 
         readBufferThread.start();
-        interruptBufferThread.start();
-
-        // Busy waiting di 1 secondo
-        time1 = new Timestamp(System.currentTimeMillis());
-        time2 = new Timestamp(System.currentTimeMillis());
-
-        time1millis = time1.getTime();
-        time2millis = time2.getTime();
-
-        while (diffTimeMillis < 1000) {
-            time2 = new Timestamp(System.currentTimeMillis());
-            time2millis = time2.getTime();
-            diffTimeMillis = time2millis - time1millis;
+        while (!readBufferThread.isInterrupted()) {
+            // Busy waiting
+            ;
         }
 
-        readBufferThread.interrupt();
-        interruptBufferThread.interrupt();
-
-        // Busy waiting di 200 ms
-        time1 = new Timestamp(System.currentTimeMillis());
-        time2 = new Timestamp(System.currentTimeMillis());
-
-        time1millis = time1.getTime();
-        time2millis = time2.getTime();
-
-        while (diffTimeMillis < 200) {
-            time2 = new Timestamp(System.currentTimeMillis());
-            time2millis = time2.getTime();
-            diffTimeMillis = time2millis - time1millis;
-        }
-
-        // Verifichiamo che i thread siano interrotti
-        if (readBufferThread.isInterrupted()) {
-            System.out.println("Il thread 'readBufferThread' è interrotto");
-        }
-        else {
-            System.out.println("Il thread 'readBufferThread' non è interrotto");
-        }
-        if (interruptBufferThread.isInterrupted()) {
-            System.out.println("Il thread 'interruptBufferThread' è interrotto");
-        }
-        else {
-            System.out.println("Il thread 'interruptBufferThread' non è interrotto");
-        }
-
-        return stringList;
+        return stringheEstratte;
     }
 
-    protected abstract void avviaTerminale() throws IOException;
+    private void stampaStringList(LinkedList<String> stringList) {
+        while (stringList.size() > 0) {
+            System.out.println(stringList.remove());
+        }
+    }
 }
