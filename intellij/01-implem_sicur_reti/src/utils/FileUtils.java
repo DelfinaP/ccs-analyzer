dcbf540a82f7f056c11459ccdc8810a1f6869bba
Alessandro Polisena <ale.polisena[at]gmail.com>
2020-02-17 13:19:31 +0100

    Begun implementation of methods for creation and deletion of directories

--- intellij/01-implem_sicur_reti/src/utils/FileUtils.java
+++ intellij/01-implem_sicur_reti/src/utils/FileUtils.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.io.File;
+
 public class FileUtils {
     public static String substituteSpacesWithEscapeChars(String originalString) {
         String modifiedString = "";
@@ -16,5 +18,37 @@ public class FileUtils {
         }
 
         return modifiedString;
+    }
+
+    public static void createDirectory(String dirPath) {
+        String absoluteDirPath = getAbsoluteDirPath();
+
+        new File(dirPath).mkdirs();
+    }
+
+
+
+    public static void deleteDirectory(String dirPath) {
+        String absoluteDirPath = getAbsoluteDirPath();
+
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                if (! Files.isSymbolicLink(f.toPath())) {
+                    deleteDir(f);
+                }
+            }
+        }
+        file.delete();
+    }
+
+    /**
+     * Converts a relative path of a directory to the corresponding absolute path. The relative path is
+     * assumed to be based on the Java project main directory.
+     * @param   relativePath   The relative path of the directory.
+     * @return   The absolute path of the directory.
+     */
+    private String getAbsoluteDirPath(String relativePath) {
+        
     }
 }
